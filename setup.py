0d032424f90102adf1407d4a1dde3f69e943bd81
Ghislain Vieilledent <ghislain.vieilledent@cirad.fr>
2020-02-03 16:18:37 +0100

    Minore changes

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ setup(name="pywdpa",
       python_requires=">=2.7",
       packages=["pywdpa"],
       package_dir={"pywdpa": "pywdpa"},
-      install_requires=["numpy", "gdal", "json", "requests", "dotenv"],
+      install_requires=["numpy", "gdal", "requests", "dotenv"],
       zip_safe=False)
 
 # End
