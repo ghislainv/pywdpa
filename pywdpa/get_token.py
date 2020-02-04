a0740508acfff655e2aca5b749820c5ca7392af4
Ghislain Vieilledent <ghislain.vieilledent@cirad.fr>
2020-02-04 16:32:50 +0100

    CI Travis

--- pywdpa/get_token.py
+++ pywdpa/get_token.py
@@ -47,14 +47,14 @@ def get_token(key="WDPA_KEY"):
     if wdpa_key is None:
         return ("\nMissing WDPA API token. Please ensure that:\n" +
                 "1) You completed this form [https://api.protectedplanet.net/request] to get the token,\n" +
-                "2) You stored the value as an environment variable with the name WDPA_KEY.")
+                "2) You stored the value as an environment variable with the recommended name WDPA_KEY.")
 
     response = requests.get("https://api.protectedplanet.net/test?token=" + wdpa_key)
 
     if (response.status_code == 401):
-        return ("\nInvalid WDPA API token. Please ensure:\n" +
-                " 1) You completed this form [https://api.protectedplanet.net/request] to get the token,\n" +
-                " 2) You stored the value in the .Renviron with the name WDPA_KEY.")
+        return ("\nInvalid WDPA API token. Please ensure that:\n" +
+                "1) You completed this form [https://api.protectedplanet.net/request] to get the token,\n" +
+                "2) You stored the value as an environment variable with the recommended name WDPA_KEY.")
 
     if (response.status_code != 200):
         return "\nSomething go wrong with your API token."
---
+++ test/test_get_token.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# ==============================================================================
+# author          :Ghislain Vieilledent
+# email           :ghislain.vieilledent@cirad.fr, ghislainv@gmail.com
+# web             :https://ecology.ghislainv.fr
+# python_version  :>=2.7
+# license         :GPLv3
+# ==============================================================================
+
+# Import
+import os
+import pywdpa
+
+def test_get_token():
+  token = pywdpa.get_token()
+  r = ["\nMissing WDPA API token. Please ensure that:\n",
+       "1) You completed this form [https://api.protectedplanet.net/request] to get the token,\n",
+       "2) You stored the value as an environment variable with the recommended name WDPA_KEY."]
+  assert token == "".join(r)
+
+# End
